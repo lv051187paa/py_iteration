da25d7f2b4394e4198671d435427471c82b14c7f
Andrii Pavliuk <31381969+lv051187paa@users.noreply.github.com>
2020-12-16 00:21:25 +0200

    solution

--- iteration.py
+++ iteration.py
@@ -1,6 +1,20 @@
+"""Sinus calculation module"""
 import math
 
 
-def sin_(x: float) -> float:
+def sin_(angle: float) -> float:
+    """returns sinus"""
+    result_prescision: int = 9
+    counter: int = 0
+    result: float = 0
+    for iterator in range(result_prescision):
+        if iterator == 1:
+            result = angle
+        if iterator > 1 and iterator % 2 == 1:
+            if not counter % 2:
+                result -= (angle**3/math.factorial(iterator))
+            else:
+                result += (angle ** 3 / math.factorial(iterator))
+            counter += 1
 
-    return 42.
+    return (round(result * 10)) / 10
